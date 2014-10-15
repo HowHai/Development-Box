a4b0371b8aa52246be347495e341555a9d564b12
Hai Nguyen <hmn91733@gmail.com>
2014-10-15 14:07:46 -0700

    Add start ionic

--- start.sh
+++ start.sh
@@ -7,6 +7,7 @@ app_name=$2
 
 # Check to make sure app type and name exists.
 if [[ -z "$1" ]]; then
+  echo "Available app types: rails, ionic, angular"
   printf "Please enter application type: "
   read app_type
 fi
@@ -22,9 +23,18 @@ function start_rails(){
   vagrant ssh -c "cd /vagrant/$app_name && bundle && bundle exec rake db:create && bundle exec rake db:migrate && rails s -p 3000"
 }
 
+function start_ionic(){
+  vagrant up
+
+  vagrant ssh -c "cd /vagrant/$app_name && ionic serve 5000 5001"
+}
+
 
 case "$app_type" in
   "rails" )
     start_rails
+    ;;
+  "ionic" )
+    start_ionic
     ;;
 esac
